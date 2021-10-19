542fd40c89f2a42a5fdd33ffa8e07d94becce73f
Katie Picha <Katie_Picha1@baylor.edu>
2021-10-18 19:20:55 -0500

    resetting the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,7 +85,17 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
         '''Start a new game when the player clicks Play.'''
         if self.play_button.rect.collidepoint(mouse_pos): # check whether the point of mouse click overlaps the rect of Play button
+            # Reset the game statistics.
+            self.stats.reset_stats() # reset the game statistics, which gives the player 3 new ships
             self.stats.game_active = True # game_active is True -> game begins!
+
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty() # empty groups
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship.
+            self.__create_fleet()
+            self.ship.center_ship()
 
     def __check_keydown_events(self, event):
         '''Respond to keypresses.'''
